a63dfbe74d507fc61c3e2b055ff38c7a91e1016d
Ubuntu <ubuntu@ip-172-31-15-22.ap-south-1.compute.internal>
2023-09-16 06:11:39 +0000

    dlete and set active

--- doctors/urls.py
+++ doctors/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
 
     re_path('patients', views.getPatients),
     path('patient/<int:id>', views.editPatient, name='edit-patient'),
+    path('patient/<int:id>', views.deletePatient, name='delete-patient'),
+    path('patient/<int:id>/toggle-active', views.togglePatientActive, name='toggle-patient-active'),
 
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -69,3 +69,46 @@ def editPatient(request, id):
     return Response("Error", status=status.HTTP_401_UNAUTHORIZED)
 
 
+
+@api_view(['DELETE'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def deletePatient(request, id):
+    if request.user.role == 2:
+        try:
+            # Get the patient from the database
+            patient = Account.objects.get(id=id)
+
+            # Update patient fields with data from the request
+            # Save the updated patient data
+            
+            return Response("Patient Deleted {}".format(patient.username))
+        except Account.DoesNotExist:
+            return Response("Patient not found", status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response("Error: {}".format(str(e)), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response("Error", status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['PATCH'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def togglePatientActive(request, id):
+    if request.user.role == 2:
+        try:
+            # Get the patient from the database
+            patient = Account.objects.get(id=id)
+
+            # Update patient fields is_active to opposite
+            patient.is_active = not patient.is_active
+            # Save the updated patient data
+            patient.save()
+            
+            return Response("Patient set to  {}".format(patient.is_active))
+        except Account.DoesNotExist:
+            return Response("Patient not found", status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response("Error: {}".format(str(e)), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response("Error", status=status.HTTP_401_UNAUTHORIZED)
+
+
